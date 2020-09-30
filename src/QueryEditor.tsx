9315e5fd450b83ed211b05921c07deb19c63bf7e
Ken Faulkner <ken.faulkner@gmail.com>
2020-09-30 12:00:37 +1000

    remove unneeded query text

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,12 +1,8 @@
-import defaults from 'lodash/defaults';
 
 import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from './DataSource';
-import { defaultQuery, MyDataSourceOptions, MyQuery } from './types';
-
-const { FormField } = LegacyForms;
+import { MyDataSourceOptions, MyQuery } from './types';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
@@ -24,18 +20,10 @@ export class QueryEditor extends PureComponent<Props> {
   };
 
   render() {
-    const query = defaults(this.props.query, defaultQuery);
-    const { queryText } = query;
 
     return (
       <div className="gf-form">
-        <FormField
-          labelWidth={8}
-          value={queryText || ''}
-          onChange={this.onQueryTextChange}
-          label="Query Text"
-          tooltip="Not used yet"
-        />
+
       </div>
     );
   }
